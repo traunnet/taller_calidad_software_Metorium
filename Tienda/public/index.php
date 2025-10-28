403f5806695abfd9026fbf64eccdb9b1bacfaa00
Juan_Hernandez <juanpablow820@gmail.com>
2025-10-28 09:23:30 -0500

    Refactor carga del archivo de mantenimiento y mejorar la inicialización de Laravel

--- Tienda/public/index.php
+++ Tienda/public/index.php
@@ -4,16 +4,24 @@ use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-$maintenanceFile = __DIR__ . '/../storage/framework/maintenance.php';
-if (file_exists($maintenanceFile)) {
-    include_once $maintenanceFile;
+
+function loadMaintenanceFile(): void
+{
+    $maintenanceFile = __DIR__ . '/../storage/framework/maintenance.php';
+    if (file_exists($maintenanceFile)) {
+        include_once $maintenanceFile;
+    }
 }
 
+function bootstrapLaravel(): \Illuminate\Foundation\Application
+{
+    require_once __DIR__ . '/../vendor/autoload.php';
+    return include_once __DIR__ . '/../bootstrap/app.php';
+}
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
+loadMaintenanceFile();
 
 /** @var Application $app */
-$app = include_once __DIR__ . '/../bootstrap/app.php';
+$app = bootstrapLaravel();
 
 $app->handleRequest(Request::capture());
